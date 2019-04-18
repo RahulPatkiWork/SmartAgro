c9f23c4f2669f1ac25d53fe8af4932805084f37a
kcajG <rahulpatki9@gmail.com>
2019-04-19 05:02:06 +0530

    Updated code for both the recommendationEngines for remote execution.Updated test code

--- machineLearning/WeatherSoilBasedCropPrediction/code/test.py
+++ machineLearning/WeatherSoilBasedCropPrediction/code/test.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 import csv
 import numpy as np
 import matplotlib.pyplot as plt
@@ -12,22 +10,22 @@ X = dataset.iloc[:, :-1].values
 Y = dataset.iloc[:, 3].values
 
 """Encoding categorical data
-
 Encoding the Independent Variable
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-
 labelencoder_X = LabelEncoder()
 X[:, 3] = labelencoder_X.fit_transform(X[:, 3])
 onehotencoder = OneHotEncoder(categorical_features = [3]) ##why str needs to be at this loc only and not first?
 X = onehotencoder.fit_transform(X).toarray()
-
 dummy var trap removal
 X=X[:,1:]  ##no need to do this manually lib does it by default..written just for info.
 """
 
+from sklearn.model_selection import cross_val_score
+lr = LinearRegression()
+lr.fit(X, Y)
+cscores = cross_val_score(lr, X, Y, cv=5)
+print('Cross Val Score for Regression is ' + str(round(cscores.mean()*100, 2))+', Accuracy score is: '+str(round(lr.score(X,Y)*100, 2)))
 
-#regressor = LinearRegression()
-#regressor.fit(X, Y)
 from sklearn.model_selection import train_test_split
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.1, random_state = 0)
 
@@ -38,3 +36,4 @@ regressor.fit(X_train, Y_train)
 
 Y_pred = regressor.predict(X_test)
 #print(Y_pred)
+
